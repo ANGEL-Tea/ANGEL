548f303f17ed088fb9ed614c3f8e8a275b520450
ANGEL-Tea <77549759+ANGEL-Tea@users.noreply.github.com>
2021-06-21 05:40:18 +0300

    Update ANGEL.lua

--- ANGEL.lua
+++ ANGEL.lua
@@ -4929,6 +4929,25 @@ send(msg.chat_id_, msg.id_,'✬︙عـليك الاشـتࢪاك في قنـاة
 end
 return false
 end
+if text == "ترتيب الاوامر" and Constructor(msg) then
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":ا","ايدي")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"ا")
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":م","رفع مميز")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"م")
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":اد","رفع ادمن")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"اد")
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":مد","رفع مدير")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"مد")
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":من","رفع منشئ")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"من")
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":اس","رفع منشئ اساسي")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"اس")
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":تعط","تعطيل الايدي بالصوره")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"تعط")
+database:set(bot_id.."ANGEL:Set:Cmd:Group:New1"..msg.chat_id_..":تفع","تفعيل الايدي بالصوره")
+database:sadd(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_,"تفع")
+send(msg.chat_id_, msg.id_,"⌔︙تم ترتيب الاوامر بالشكل التالي ~\n- ايدي - ا .\n- مميز - م .\n- ادمن - اد .\n- مدير - مد . \n- منشى - من . \n- المنشئ الاساسي - اس  . \n- تعطيل الايدي بالصوره - تعط .\n- تفعيل الايدي بالصوره - تفع .")  
+end					
 local list = database:smembers(bot_id.."ANGEL:List:Cmd:Group:New"..msg.chat_id_.."")
 t = "✬︙قائمه الاوامر المضافه  \n — — — — — — — — — \n"
 for k,v in pairs(list) do
